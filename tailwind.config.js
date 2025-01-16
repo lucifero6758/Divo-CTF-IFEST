fe982d085cb790e5b1ca173ae49addb0c004ae5c
lucifero6758 <xtertyx223@gmail.com>
2025-01-16 08:13:37 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        quantico: ["Quantico", "serif"],
+        jersey: ["'Jersey 10'", "serif"],
+        jersey25: ["'Jersey 25'", "serif"],
+        roboto: ["Roboto", "serif"],
+        khdam: ["'Kdam Thmor Pro'", "serif"],
+        inter: ["'Inter Tight'", "serif"],
+        montserrat: ["Montserrat", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
