d656de3193c2f5f888dbca2c429fa0fc9fba24b1
lucifero6758 <xtertyx223@gmail.com>
2025-02-03 09:23:03 +0800

    sizing timeline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        quantico: ["Quantico", "serif"],
-        jersey: ["'Jersey 10'", "serif"],
-        jersey25: ["'Jersey 25'", "serif"],
-        roboto: ["Roboto", "serif"],
-        khdam: ["'Kdam Thmor Pro'", "serif"],
-        inter: ["'Inter Tight'", "serif"],
-        montserrat: ["Montserrat", "serif"],
-      },
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                quantico: ["Quantico", "serif"],
+                jersey: ["'Jersey 10'", "serif"],
+                jersey25: ["'Jersey 25'", "serif"],
+                roboto: ["Roboto", "serif"],
+                khdam: ["'Kdam Thmor Pro'", "serif"],
+                inter: ["'Inter Tight'", "serif"],
+                montserrat: ["Montserrat", "serif"],
+                interBold: ["Inter", "serif"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
